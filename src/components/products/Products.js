6f7e04fe47e62e5cc0ec1ddee725d855fbd292db
AlexOros <alexanderbarber1986@gmail.com>
2020-03-05 09:11:24 +0200

    added pagination , need to fix starting number bug and add to cart bug

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,9 +1,34 @@
-import React, { useEffect, useContext } from "react"
+import React, { useEffect, useContext, useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 
+import Pagination from "@material-ui/lab/Pagination"
+import usePagination from "../../hooks/usePagination"
 import { ProductsContext } from "../../context"
 import { Product } from "../../components"
 import StyledProducts from "./Products.style"
+
+const query = graphql`
+  query ProductsQuery {
+    allStrapiProduct {
+      nodes {
+        title
+        price
+        old_price
+        stock
+        image {
+          childImageSharp {
+            fluid(maxWidth: 900, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+            fixed(width: 200, height: 200) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Products = () => {
   const { products, handleSetProducts, handleAddProductToCart } = useContext(
@@ -11,43 +36,37 @@ const Products = () => {
   )
   const {
     allStrapiProduct: { nodes },
-  } = useStaticQuery(graphql`
-    query ProductsQuery {
-      allStrapiProduct {
-        nodes {
-          title
-          price
-          old_price
-          stock
-          image {
-            childImageSharp {
-              fluid(maxWidth: 900, quality: 100) {
-                ...GatsbyImageSharpFluid
-              }
-              fixed(width: 200, height: 200) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
+  } = useStaticQuery(query)
+
+  const { page, maxPages, handleChangePage } = usePagination({
+    items: products,
+    itemsPerPage: 2,
+  })
 
   useEffect(() => {
     handleSetProducts(nodes)
   }, [handleSetProducts, nodes])
 
   return (
-    <StyledProducts>
-      {products.map(product => (
-        <Product
-          key={product.id}
-          product={product}
-          handleAddProductToCart={handleAddProductToCart}
+    <div>
+      <div>
+        <Pagination
+          onChange={handleChangePage}
+          size="large"
+          count={maxPages}
+          color="secondary"
         />
-      ))}
-    </StyledProducts>
+      </div>
+      <StyledProducts>
+        {page.map(product => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddProductToCart={handleAddProductToCart}
+          />
+        ))}
+      </StyledProducts>
+    </div>
   )
 }
 
