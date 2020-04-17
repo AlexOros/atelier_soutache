6377743c0684432a30bdbfed62f197f1308bbf01
AlexOros <alexanderbarber1986@gmail.com>
2020-04-17 14:13:57 +0300

    Uninstalled paypal

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,7 @@ import { I18nextProvider } from "react-i18next"
 import { MuiThemeProvider, responsiveFontSizes } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { ThemeProvider } from "styled-components"
-import { Helmet } from "react-helmet"
+
 import { ApolloProvider } from "@apollo/react-hooks"
 
 import { client } from "./src/api/apollo"
@@ -15,9 +15,6 @@ import { Layout } from "./src/components"
 import i18next from "./src/api/i18n"
 
 const responsiveTheme = responsiveFontSizes(theme)
-
-const CLIENT_ID =
-  "AYTJuDuNNIUA9GFu9y8pHe25JAUqIQEPT6Rk2jzVmVK4Flf64YDaEAyacNCu3bQd0OGOLN4Zrr_8AKZw"
 
 export const wrapRootElement = ({ element }) => {
   return (
@@ -32,11 +29,6 @@ export const wrapRootElement = ({ element }) => {
             </ProductsProvider>
           </ThemeProvider>
         </MuiThemeProvider>
-        <Helmet>
-          <script
-            src={`https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}&currency=EUR&locale=en_RO`}
-          ></script>
-        </Helmet>
       </I18nextProvider>
     </ApolloProvider>
   )
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -13,6 +13,7 @@ const query = graphql`
     allStrapiProduct(filter: { show_product: { eq: true } }) {
       nodes {
         title
+        strapiId
         price
         old_price
         stock
